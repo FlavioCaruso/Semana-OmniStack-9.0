266ea416d42272e2c68edbe11e0b30ea2840397e
Flavio Berbel Caruso <flavio.caruso@butantan.gov.br>
2019-10-04 15:17:14 -0300

    Criado a tela de login

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,8 +1,74 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, KeyboardAvoidingView, Image, StyleSheet, Text, TouchableOpacity, TextInput } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function Login(){
-  return <View>
-    <Text>Ola</Text>
-  </View>
+  return (
+  <KeyboardAvoidingView  behavior="padding" style={styles.container}>
+    <Image source={logo}/>
+    <View style={styles.form}>  
+      <Text style={styles.label}>Seu Email *</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Seu-email"
+        placeholderTextColor="#999"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+
+      <Text style={styles.label}>Tecnologias *</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Tecnologias de Interesse"
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Encontrar spots</Text>
+      </TouchableOpacity>
+    </View>
+  </ KeyboardAvoidingView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  form:{
+    alignSelf: 'stretch',
+    paddingHorizontal: 30,
+    marginTop: 30,
+  },
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    marginBottom: 20,
+    borderRadius: 2
+  },
+  button:{
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
+})
